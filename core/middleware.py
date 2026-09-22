1462973a070ee8859238a3b81cd64c876b9e725c
root <root@srv1154223.hstgr.cloud>
2026-09-22 17:34:22 +0000

    custom domain obidaamin.com

--- core/middleware.py
+++ core/middleware.py
@@ -18,7 +18,7 @@ class CustomDomainMiddleware:
     SKIP_PREFIXES = (
         '/static/',
         '/media/',
-        '/image-thumb/',
+        '/image-thumb/', 
         '/api/',
         '/admin/',
         '/accounts/',
@@ -172,8 +172,6 @@ class LanguagePreferenceMiddleware:
         '/ar/payment/success/': '/payment/success/',
         '/payment/failure/': '/ar/payment/failure/',
         '/ar/payment/failure/': '/payment/failure/',
-        '/dashboard/customize/': '/ar/dashboard/customize/',
-        '/ar/dashboard/customize/': '/dashboard/customize/',
     }
 
     def __init__(self, get_response):
